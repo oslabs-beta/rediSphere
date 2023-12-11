9c1a3e4cd408f40894705223a233e3fd28f66925
Jason Wong <wongj003@gmail.com>
2023-12-11 14:29:50 -0500

    setting up file skeleton

---
+++ client/App.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+const App = () => {
+  return <div className="test">Hello World</div>;
+};
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  //   mode: 'development',
+  entry: {
+    src: './client/index.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env', '@babel/react'],
+        },
+      },
+      {
+        test: /\.s?css/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  //injects script tags
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'index.html',
+    }),
+  ],
+  //configures webpack dev server & proxies calls to /api to root of backend
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    historyApiFallback: true,
+    hot: true,
+    static: {
+      publicPath: '/build',
+      directory: path.resolve(__dirname, 'build'),
+    },
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    proxy: {
+      '/api/**': 'http://localhost:3000',
+    },
+  },
+};
