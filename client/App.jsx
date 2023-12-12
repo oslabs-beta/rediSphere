eb005026df8b1da422d2b77d46458f9e8d396d56
Michelle Xie <mxie97@gmail.com>
2023-12-11 17:02:02 -0800

    build out dummy front end including login page, sign up page, and dashboard with d3 line plot example

--- client/App.jsx
+++ client/App.jsx
@@ -7,13 +7,6 @@ import SignupPage from './components/SignupPage.jsx';
 import DashBoard from './components/DashBoard.jsx';
 
 const App = () => {
-  // const [data, setData] = useState(() => d3.ticks(-2, 2, 200).map(Math.sin));
-
-  // function onMouseMove(event) {
-  //   const [x, y] = d3.pointer(event);
-  //   setData(data.slice(-200).concat(Math.atan2(x, y)));
-  // }
-
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
