4dfea53265dae8195c7ec264e6eb0ff346fcbf1a
Jason Wong <wongj003@gmail.com>
2023-12-13 17:12:13 -0500

    added redis connection to server and connected the apiroutes/controller middleware

--- server/controllers/redisController.js
+++ server/controllers/redisController.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const Redis = require('redis');
-const redisClient = Redis.createClient();
+// const Redis = require('redis');
+// const redisClient = Redis.createClient();
 const redisController = {};
 
+//efficiency of cache usage metric
 redisController.getCacheHitsRatio = async (req, res, next) => {
+  //store Redis client from middleware
+  const redisClient = req.redisClient;
   //connect to local redis instannce
-  await redisClient.connect();
+  // await redisClient.connect();
   // //set one key to get *some* data to see the performance on
   // await redisClient.set('test', 'hello');
   //response is a giant array of comma and newline separated values, with sections delinated by '# <SectionHeader>',
@@ -18,6 +21,32 @@ redisController.getCacheHitsRatio = async (req, res, next) => {
   cacheMisses = Number(cacheMisses.slice(cacheMisses.indexOf(':') + 1));
   console.log('hits', cacheHits);
   console.log('misses', cacheMisses);
+  //if ratio lower than -0.8,  then a significant amount of the requested keys are evicted, expired, or do not exist at all
+  if (cacheHits + cacheMisses === 0) res.locals.cacheHitRatio = 0;
+  else res.locals.cacheHitRatio = cacheHits / (cacheHits + cacheMisses);
+  console.log('cachehitratio', res.locals.cacheHitRatio);
+  return next();
+};
+
+//A persistent positive value of this metric is an indication that you need to scale the memory up.
+//A positive metric value shows that your expired data is being cleaned up properly
+redisController.getEvictedExpired = async (req, res, next) => {
+  //store Redis client from middleware
+  const redisClient = req.redisClient;
+  //connect to local redis instannce
+  // await redisClient.connect();
+  // //set one key to get *some* data to see the performance on
+  // await redisClient.set('test', 'hello');
+  //response is a giant array of comma and newline separated values, with sections delinated by '# <SectionHeader>',
+  const stats = await redisClient.info('stats');
+  //separate string into individual metrics and store in array
+  const metrics = stats.split('\r\n');
+  let expired = metrics.find((str) => str.startsWith('expired_keys'));
+  let evicted = metrics.find((str) => str.startsWith('evicted_keys'));
+  expired = Number(expired.slice(expired.indexOf(':') + 1));
+  evicted = Number(evicted.slice(evicted.indexOf(':') + 1));
+  console.log('expired caches', expired);
+  console.log('evicted caches', evicted);
   return next();
 };
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Redis, createClient } = require('redis');
+const { createClient } = require('redis');
 //import 'express-async-errors'
 //const cors = require('cors');
 //const cookieSession = require('cookie-session');
@@ -11,36 +11,17 @@ const PORT = process.env.PORT;
 const redisPassword = process.env.REDIS_PASS;
 // console.log(process.env.REDIS_PASS);
 const socketHost = process.env.HOST;
-const redisPort = 17853;
+const redisPort = process.env.REDIS_PORT;
+const redisUser = process.env.REDIS_USER;
+const redisURL = `redis://${redisUser}:${redisPassword}@${socketHost}:${redisPort}`;
 
 //temp before creating router
-const statsController = require('./controllers/statsController.js');
-
-//process.env.PORT;
-// const redisPassword = process.env.REDIS_PASS;
-// const socketHost = process.env.HOST;
-// const redisPort = 17853;
-
-//creating a connection to redis instance
-// const redisClient = Redis.createClient({
-//   //redis[s]://[[username][:password]@][host][:port][/db-number]
-//   //url: 'redis://alice:foobared@awesome.redis.server:6380'
-//   //   password: redisPassword,
-//   password: 'GJ2F0obKIJEQiCwR3ci03V6qLr8CFkJY',
-//   socket: {
-//     // host: socketHost,
-//     host: 'redis-17853.c326.us-east-1-3.ec2.cloud.redislabs.com',
-//     port: redisPort,
-//   },
-// });
-//const redisClient = Redis.createClient();
-// redisClient.connect()
+// const statsController = require('./controllers/statsController.js');
 
 const app = express();
 
 // Body parser middleware for JSON data
 app.use(express.json());
-
 // Body parser middleware for URL-encoded data
 app.use(express.urlencoded({ extended: true }));
 
@@ -53,6 +34,35 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+//creating a connection to redis instance
+const redisClient = createClient({
+  //   //redis[s]://[[username][:password]@][host][:port][/db-number]
+  //   //url: 'redis://alice:foobared@awesome.redis.server:6380'
+  password: redisPassword,
+  //   password: 'GJ2F0obKIJEQiCwR3ci03V6qLr8CFkJY',
+  socket: {
+    host: socketHost,
+    // host: 'redis-17853.c326.us-east-1-3.ec2.cloud.redislabs.com',
+    port: redisPort,
+  },
+});
+//Open connection to Redis Instance
+redisClient.connect().catch((err) => {
+  console.log(`Error connecting to Redis Server: ${err}`);
+});
+//listening for Redis connection events
+redisClient.on('connect', () => {
+  console.log(`Connected to Redis Server: ${socketHost} on port ${redisPort}`);
+});
+redisClient.on('error', (err) => {
+  console.log(`Error connecting to Redis Server: ${err}`);
+});
+//Middleware to pass Redis instance to /api
+app.use((req, res, next) => {
+  req.redisClient = redisClient;
+  return next();
+});
+
 app.use('/api', apiRouter);
 
 // app.get('/', (req, res) => {
@@ -62,7 +72,7 @@ app.use('/api', apiRouter);
 // catch-all route handler for any requests to an unknown route
 app.use((req, res) => res.status(404).send("This is not the page you're looking for..."));
 
-//express error handler (middleware)
+//express global error handler (middleware)
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
