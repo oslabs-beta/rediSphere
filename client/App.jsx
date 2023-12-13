38fd6414e0a428db716c22ee33eebe644565c9d8
Jake Kunkel <therealjakekunkel@gmail.com>
2023-12-13 17:43:09 -0500

    add barebones ping for data

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './styles.css';
 import LoginPage from './components/LoginPage.jsx';
@@ -7,6 +7,38 @@ import SignupPage from './components/SignupPage.jsx';
 import DashBoard from './components/DashBoard.jsx';
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    //setInteral takes:
+    //1. Callback to execute
+    //2. interval to wait between executions in ms
+    const interval = setInterval(
+      () => {
+        //call to backend
+        fetch('/api')
+          //grab data from response
+          .then((res) => {
+            res.json();
+            //add'l parsing??
+          })
+          .then((newPoint) => {
+            //add new datapoint to state
+            setData((prevData) => [...prevData, newPoint]);
+          });
+      },
+      1000, //frequency in milliseconds
+    );
+    // a setInterval object will run until you actually tell it to stop.
+    //in this case, we want it to stop if / when we unmount the component
+    //consider how we can set it differently if needed
+
+    //TODO: set a conditional so that the useEffect only runs when we have a valid database connection to Redis active -- avoid erroring out/timeout
+    // check w/Michelle re:auth and credentials to set strat for this
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
--- client/components/DashBoard.jsx
+++ client/components/DashBoard.jsx
@@ -28,8 +28,17 @@ const DashBoard = () => {
     );
     // a setInterval object will run until you actually tell it to stop.
     //in this case, we want it to stop if / when we unmount the component
-    //consider how we can set it differently if needed --but likely only while the graphs are rendering do we need to do this?
-  });
+    //consider how we can set it differently if needed
+
+    //eg, we want to stay fetching data as long as the user is in the app (not just while the dashboard has mounted), probably,
+    //set up the useEffect on the app component
+
+    //TODO: set a conditional so that the useEffect only runs when we have a valid database connection to Redis active -- avoid erroring out/timeout
+    // check w/Michelle re:auth and credentials to set strat for this
+
+    return () => clearInterval(interval);
+  }, []); //passing an empty dependency array here so the effect will run once on startup, and then on teardown (the return fires) stop trying to ping the backend
+  //TO-DO:
 
   // const [data, setData] = useState(() => d3.ticks(-2, 2, 200).map(Math.sin));
 
