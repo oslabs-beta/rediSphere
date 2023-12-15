406dfe1bbfb6fba1121550b432c6ded89449fc2d
Jason Wong <wongj003@gmail.com>
2023-12-14 19:50:38 -0500

    added latency and memory metrics

--- server/controllers/redisController.js
+++ server/controllers/redisController.js
@@ -23,7 +23,9 @@ redisController.getCacheHitsRatio = async (req, res, next) => {
   console.log('misses', cacheMisses);
   //if ratio lower than -0.8,  then a significant amount of the requested keys are evicted, expired, or do not exist at all
   if (cacheHits + cacheMisses === 0) res.locals.cacheHitRatio = 0;
-  else res.locals.cacheHitRatio = cacheHits / (cacheHits + cacheMisses);
+  else {
+    res.locals.cacheHitRatio = cacheHits / (cacheHits + cacheMisses);
+  }
   console.log('cachehitratio', res.locals.cacheHitRatio);
   return next();
 };
@@ -49,6 +51,47 @@ redisController.getEvictedExpired = async (req, res, next) => {
   console.log('expired caches', expired);
   res.locals.evictedExpired = { evicted: evicted, expired: expired };
   console.log(res.locals.evictedExpired);
+  return next();
+};
+
+redisController.getResponseTimes = async (req, res, next) => {
+  //store Redis client from middleware
+  const redisClient = req.redisClient;
+
+  const stats = await redisClient.info('stats');
+  const metrics = stats.split('\r\n');
+  let commandsProcessed = metrics.find((str) => str.startsWith('total_commands_processed'));
+  commandsProcessed = Number(commandsProcessed.slice(commandsProcessed.indexOf(':') + 1));
+
+  const cmdstats = await redisClient.info('commandstats');
+  const cmdmetrics = cmdstats.split('\r\n');
+  let avgGetCacheTime = cmdmetrics.find((str) => str.startsWith('cmdstat_get'));
+  // time is in microseconds
+  avgGetCacheTime = Number(avgGetCacheTime.slice(avgGetCacheTime.indexOf('usec_per_call=') + 14));
+  res.locals.latency = { commandsProcessed: commandsProcessed, avgGetCacheTime: avgGetCacheTime };
+  //latency, needs to be enabled
+  // const st = await redisClient.info('latencystats');
+
+  return next();
+};
+//memory usage
+redisController.getMemory = async (req, res, next) => {
+  //store Redis client from middleware
+  const redisClient = req.redisClient;
+
+  const stats = await redisClient.info('memory');
+  // console.log(stats);
+  const metrics = stats.split('\r\n');
+  //the memory allocated to Redis, size of cache data
+  let usedMemory = metrics.find((str) => str.startsWith('used_memory_human'));
+  //max amount of memory Redis has consumed.
+  let peakUsedMemory = metrics.find((str) => str.startsWith('used_memory_peak_human'));
+  //the number is in MB
+  usedMemory = Number(usedMemory.slice(usedMemory.indexOf(':') + 1, usedMemory.length - 1));
+  peakUsedMemory = Number(
+    peakUsedMemory.slice(peakUsedMemory.indexOf(':') + 1, peakUsedMemory.length - 1),
+  );
+  res.locals.memory = { usedMemory: usedMemory, peakUsedMemory: peakUsedMemory };
   return next();
 };
 
