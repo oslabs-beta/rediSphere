fb648518c43b17f51af301b12d189d104f507afb
eduardo-uribe <ed2711uribe@gmail.com>
2023-12-20 09:58:40 -0700

    Update landing page styles

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -7,8 +7,10 @@ const HomePage = () => {
     <div className="home-page">
       <Header />
       <div className="main-content">
-        <h1>Discover critical Redis performance issues in real-time</h1>
-        <p>
+        <h1 className="main-content-title">
+          Discover critical Redis performance issues in real-time
+        </h1>
+        <p className="main-content-description">
           Use CacheApp and discover critical performance issues in real-time. View your latency and
           cache-hit-misses all in one place and minimize unpleasant user experiences.
         </p>
