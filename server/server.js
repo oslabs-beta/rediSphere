d5a8bdefb4f85c7febdc64d945a984b070eea76a
Michelle Xie <mxie97@gmail.com>
2024-01-09 15:23:03 -0800

    tweaking server file

--- server/server.js
+++ server/server.js
@@ -20,18 +20,18 @@ app.use(cookieParser());
 app.use(express.json()); // parses body EXCEPT html
 app.use(express.urlencoded({ extended: true })); // requires header to parse
 
-// if (process.env.NODE_ENV === 'production') {
-// statically serve everything in the build folder on the route '/build'
-app.use('/build', express.static(path.join(__dirname, '../build')));
-// serve index.html on the route '/'
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
-});
-// }
-
 //mounting api router, redis metrics middlewares
 app.use('/api', apiRouter);
 app.use('/users', authRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  // statically serve everything in the build folder on the route '/build'
+  app.use('/build', express.static(path.join(__dirname, '../build')));
+  // serve index.html on the route '/'
+  app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+  });
+}
 
 // catch-all route handler for any requests to an unknown route
 app.use((req, res) => res.status(404).send("This is not the page you're looking for..."));
